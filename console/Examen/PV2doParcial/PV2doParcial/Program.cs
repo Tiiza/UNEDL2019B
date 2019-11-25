7f7558857f0892c08accf9dee63714b859c1b862
Isay Villarruel <isay1005@hotmail.com>
2019-11-25 10:46:36 -0600

    Codigo 2 parcial Examen

---
+++ console/Examen/PV2doParcial/PV2doParcial/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace PV2doParcial
+{
+    class Program
+    {
+
+        static void Main(string[] args) {
+            Stack paretnesis = new Stack();
+            String ecuacion;
+
+            Console.WriteLine("[-- Ingresa la ecuacion --]"); 
+            ecuacion = Console.ReadLine();
+            
+            for (int i = 0; i < ecuacion.Length; ++i)
+            {
+                if ((ecuacion[i] == '(') || (ecuacion[i] == '{') || (ecuacion[i] == '['))
+                {
+                    paretnesis.Push(ecuacion[i]);
+                }
+                else if (paretnesis.Count > 0)
+                {
+                    switch (ecuacion[i])
+                    {
+                        case ']':
+                            paretnesis.Pop();
+                            break;
+                        case '}':
+
+                            paretnesis.Pop();
+                            break;
+                        case ')':
+                            paretnesis.Pop();
+                            break;
+                    }
+                }
+            }
+            if (paretnesis.Count == 0)
+            {
+                Console.WriteLine("Ecuacion Correctamente balanceada");
+            }
+            else
+            {
+                Console.WriteLine("Ecuacion incorrectamente balanceada");
+            }
+
+
+
+
+        }
+    }
+}
